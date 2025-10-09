fe3f2070f02160f9ae38ae2a6de712132740a10a
Milan Topuzov <milan.topuzov@salewise.io>
2025-10-09 14:04:19 +0200

    [MIG] Replace _sql_constraints with models.Constraint (v19)

--- base_user_role/models/role.py
+++ base_user_role/models/role.py
@@ -156,13 +156,10 @@ class ResUsersRoleLine(models.Model):
     date_from = fields.Date("From")
     date_to = fields.Date("To")
     is_enabled = fields.Boolean("Enabled", compute="_compute_is_enabled")
-    _sql_constraints = [
-        (
-            "user_role_uniq",
-            "unique (user_id,role_id)",
-            "User roles can be assigned to a user only once at a time",
-        )
-    ]
+    _user_role_uniq = models.Constraint(
+        "UNIQUE (user_id, role_id)",
+        "User roles can be assigned to a user only once at a time",
+    )
 
     @api.depends("date_from", "date_to")
     def _compute_is_enabled(self):
